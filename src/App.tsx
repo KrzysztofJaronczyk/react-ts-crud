43c7d99d2c7a1b07bb879f1638d4355748ea722f
KrzysztofJaronczyk <krzysztofjaronczykzyt@gmail.com>
2023-11-25 22:05:06 +0100

    Fix passing elements

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ const App: React.FC = () => {
         <h1>React TypeScript CRUD App</h1>
         <Routes>
           <Route path="/" element={<UserTable />} />
-          {/* Pass the userId parameter */}
-          <Route path="/user/:id" element={<UserDetails userId={1} />} />
+          {/* Use the :id parameter from the URL */}
+          <Route path="/user/:id" element={<UserDetails />} />
         </Routes>
       </div>
     </Router>
--- src/components/UserDetails.tsx
+++ src/components/UserDetails.tsx
@@ -1,84 +1,87 @@
-// src/components/UserDetails.tsx
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
-interface UserDetailsProps {
-  userId: number;
-}
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom' // Add this import
+import axios from 'axios'
 
 interface User {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    };
-  };
+	id: number
+	name: string
+	username: string
+	email: string
+	address: {
+		street: string
+		suite: string
+		city: string
+		zipcode: string
+		geo: {
+			lat: string
+			lng: string
+		}
+	}
 }
 
-const UserDetails: React.FC<UserDetailsProps> = ({ userId }) => {
-  const [user, setUser] = useState<User | null>(null);
+const UserDetails: React.FC = () => {
+	const { id } = useParams() // Get the id parameter from the URL
+	const userId = parseInt(id ?? '', 10) // Convert id to a number
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
-        setUser(response.data);
-      } catch (error) {
-        console.error('Error fetching user:', error);
-      }
-    };
+	const [user, setUser] = useState<User | null>(null)
 
-    fetchUser();
-  }, [userId]);
+	useEffect(() => {
+		const fetchUser = async () => {
+			try {
+				const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`)
+				setUser(response.data)
+			} catch (error) {
+				console.error('Error fetching user:', error)
+			}
+		}
 
-  const handleCreate = () => {
-    console.log('Create user');
-  };
+		fetchUser()
+	}, [userId])
 
-  const handleRead = () => {
-    console.log('Read user');
-  };
+	const handleCreate = () => {
+		console.log('Create user')
+	}
 
-  const handleUpdate = () => {
-    console.log('Update user');
-  };
+	const handleRead = () => {
+		console.log('Read user')
+	}
 
-  const handleDelete = () => {
-    console.log('Delete user');
-  };
+	const handleUpdate = () => {
+		console.log('Update user')
+	}
 
-  return (
-    <div>
-      <h2>User Details</h2>
-      {user ? (
-        <div>
-          <p>ID: {user.id}</p>
-          <p>Name: {user.name}</p>
-          <p>Username: {user.username}</p>
-          <p>Email: {user.email}</p>
-          <p>Address: {user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}</p>
-          <p>Location: Lat - {user.address.geo.lat}, Lng - {user.address.geo.lng}</p>
-        </div>
-      ) : (
-        <p>Loading user data...</p>
-      )}
+	const handleDelete = () => {
+		console.log('Delete user')
+	}
 
-      <div>
-        <button onClick={handleCreate}>Create User</button>
-        <button onClick={handleRead}>Read User</button>
-        <button onClick={handleUpdate}>Update User</button>
-        <button onClick={handleDelete}>Delete User</button>
-      </div>
-    </div>
-  );
-};
+	return (
+		<div>
+			<h2>User Details</h2>
+			{user ? (
+				<div>
+					<p>ID: {user.id}</p>
+					<p>Name: {user.name}</p>
+					<p>Username: {user.username}</p>
+					<p>Email: {user.email}</p>
+					<p>
+						Address: {user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}
+					</p>
+					<p>
+						Location: Lat - {user.address.geo.lat}, Lng - {user.address.geo.lng}
+					</p>
+				</div>
+			) : (
+				<p>Loading user data...</p>
+			)}
 
-export default UserDetails;
+			<div>
+				<button onClick={handleCreate}>Create User</button>
+				<button onClick={handleRead}>Read User</button>
+				<button onClick={handleUpdate}>Update User</button>
+				<button onClick={handleDelete}>Delete User</button>
+			</div>
+		</div>
+	)
+}
+
+export default UserDetails
